26eaa9b571edda01ce495ccf1d082ec1c322adaa
Valeriy Kovalevskiy <v.kovalevskiy@vironit.co.uk>
2020-07-14 12:17:07 +0300

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Valeriy Kovalevskiy on 7/14/20.
+//  Copyright © 2020 Valeriy Kovalevskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
