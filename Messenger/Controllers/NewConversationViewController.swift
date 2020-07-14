8aea44f11ab5ac67375f26a92867a1e3f333a987
Valeriy Kovalevskiy <v.kovalevskiy@vironit.co.uk>
2020-07-14 12:32:53 +0300

    set up initial project

---
+++ Messenger/Controllers/NewConversationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewConversationViewController.swift
+//  Messenger
+//
+//  Created by Valeriy Kovalevskiy on 7/14/20.
+//  Copyright © 2020 Valeriy Kovalevskiy. All rights reserved.
+//
+
+import UIKit
+
+class NewConversationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
